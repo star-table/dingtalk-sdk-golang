51a53c945bb11f92cab1d4c8e1f0ce534842f9d7
nico <ainililia@163.com>
2019-07-05 13:33:34 +0800

    added user info api

--- api_user_caller.go
+++ api_user_caller.go
@@ -5,8 +5,19 @@ import (
 	"github.com/polaris-team/dingding-sdk-golang/json"
 )
 
-func GetUserDetail(accessToken string, userId string, lang *string) {
-
+func GetUserDetail(accessToken string, userId string, lang *string) (GetUserDetailResp, error) {
+	params := map[string]string{
+		"access_token": accessToken,
+		"userid":       userId,
+		"lang":         *lang,
+	}
+	body, err := http.Get("https://oapi.dingtalk.com/user/get", params)
+	resp := GetUserDetailResp{}
+	if err != nil {
+		return resp, err
+	}
+	json.FromJson(body, &resp)
+	return resp, err
 }
 
 func GetDepMember(accessToken string, deptId string) (GetDepMemberResp, error) {
--- api_user_models.go
+++ api_user_models.go
@@ -1,20 +1,37 @@
 package dingding_sdk_golang
 
+type BaseResp struct {
+	ErrCode int    `json:"errcode"`
+	ErrMsg  string `json:"errmsg"`
+}
+
 type GetDepMemberResp struct {
-	ErrCode int      `json:"errcode"`
-	ErrMsg  string   `json:"errmsg"`
+	BaseResp
 	UserIds []string `json:"userIds"`
 }
 
 type GetUserDetailResp struct {
-	ErrCode         int    `json:"errcode"`
-	ErrMsg          string `json:"errmsg"`
-	Name            string ``
-	UnionId         string
-	UserId          string
-	IsLeaderInDepts string
-	IsBoos          bool
-	HiredDate       int64
-	IsSenior        bool
-	Department      []int
+	BaseResp
+	Name            string           `json:"name"`
+	UnionId         string           `json:"unionid"`
+	UserId          string           `json:"userid"`
+	IsLeaderInDepts string           `json:"isLeaderInDepts"`
+	IsBoos          bool             `json:"isBoss"`
+	HiredDate       int64            `json:"hiredDate"`
+	IsSenior        bool             `json:"isSenior"`
+	Department      []int            `json:"department"`
+	OrderInDepts    string           `json:"orderInDepts"`
+	Active          bool             `json:"active"`
+	Avatar          bool             `json:"avatar"`
+	IsAdmin         bool             `json:"isAdmin"`
+	IsHide          bool             `json:"isHide"`
+	JobNumber       string           `json:"jobnumber"`
+	Position        string           `json:"position"`
+	Roles           []UserDetailRole `json:"roles"`
+}
+
+type UserDetailRole struct {
+	Id        int64  `json:"id"`
+	Name      string `json:"name"`
+	GroupName string `json:"groupName"`
 }
